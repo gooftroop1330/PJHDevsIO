e0941689a240344555bc3d95219c56bfea88c965
Preston <prestonsmith657@gmail.com>
2020-04-06 01:00:01 -0400

    email works??????

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,55 @@
-const express = require('express');
+//import modules installed at the previous step. We need them to run Node.js server and send emails
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+// create a new Express application instance 
 const app = express();
 
-/*
-CORS - Cross Origin Request Security
-localhost:3000 -> backend
-localhost:4200 ->frontend
- */
+//configure the Express middleware to accept CORS requests and parse request body into JSON
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(cors())
-
-/*
-??might need this??
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+//start application server on port 3000
+app.listen(3000, () => {
+    console.log("The server started on port 3000");
 });
-*/
-//app.use(express.json());
-app.listen(3000, () => console.log("Server Connected on port 3000"));
+
+// define a sendmail endpoint, which will send emails and response with the corresponding status
+app.post("/sendmail", (req, res) => {
+    console.log("request came");
+    let user = req.body;
+    sendMail(user, (err, info) => {
+        if (err) {
+            console.log(err);
+            res.status(400);
+            res.send({ error: "Failed to send email" });
+        } else {
+            console.log("Email has been sent");
+            res.send(info);
+        }
+    });
+});
+
+const sendMail = (user, callback) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "pjhdevs@gmail.com",
+            pass: "It'sASecret3"
+        }
+    });
+
+    transporter.sendMail(mailOptions, callback);
+}
+
+const mailOptions = {
+    from: `"PJHDevs", "pjhdevs@gmail.com"`,
+    to: "prestonsmith657@gmail.com",
+    subject: "testerrrrrrr",
+    html: "<h1>yeah boy</h1>"
+};
+
